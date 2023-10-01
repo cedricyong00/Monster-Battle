9e724c55fd083ce7a33be4790c74e973c50e78ad
cedricyong00 <cedricyong2000@gmail.com>
2023-10-01 18:16:57 +0800

    Added click event to buff buttons and buff removal

--- Monster-Battle/script.js
+++ Monster-Battle/script.js
@@ -1,14 +1,14 @@
-const potion = {
+let potion = {
     heal : 30,
     numberOfUsage : 4
 };
 
-const attackBuff = {
+let attackBuff = {
     buffAttack : 25,
     numberOfUsage : 5
 };
 
-const accuracyBuff = {
+let accuracyBuff = {
     buffAccuracy : 25,
     numberOfUsage : 5
 };
@@ -50,13 +50,13 @@ const webPage = document.querySelector('body');
 function addAttackDescription(event) { 
         const typeOfAttack = event.target.id;
     if (typeOfAttack === "spit") {
-        MsgElement.innerText = "Damage : " + spitAttack.damage + ", Accuracy : " + spitAttack.accuracy + ", Number of usage left : " + spitAttack.numberOfUsage;
+        MsgElement.innerText = "Damage : " + spitAttack.damage + ", Accuracy : " + spitAttack.accuracy + "%, Number of usage left : " + spitAttack.numberOfUsage;
     } else if (typeOfAttack === "sand") {
-        MsgElement.innerText = "Damage : " + sandAttack.damage + ", Accuracy : " + sandAttack.accuracy + ", Number of usage left : " + sandAttack.numberOfUsage;
+        MsgElement.innerText = "Damage : " + sandAttack.damage + ", Accuracy : " + sandAttack.accuracy + "%, Number of usage left : " + sandAttack.numberOfUsage;
     } else if (typeOfAttack === "earthquake") {
-        MsgElement.innerText = "Damage: " + earthquakeAttack.damage + ", Accuracy: " + earthquakeAttack.accuracy + ", Number of usage left: " + earthquakeAttack.numberOfUsage;
+        MsgElement.innerText = "Damage: " + earthquakeAttack.damage + ", Accuracy: " + earthquakeAttack.accuracy + "%, Number of usage left: " + earthquakeAttack.numberOfUsage;
     } else if (typeOfAttack === "sandstorm") {
-        MsgElement.innerText = "Damage: " + sandstormAttack.damage + ", Accuracy: " + sandstormAttack.accuracy + ", Number of usage left: " + sandstormAttack.numberOfUsage;
+        MsgElement.innerText = "Damage: " + sandstormAttack.damage + ", Accuracy: " + sandstormAttack.accuracy + "%, Number of usage left: " + sandstormAttack.numberOfUsage;
     }
 };
 
@@ -67,11 +67,11 @@ attackButton.forEach(button => {
 function addBuffDescription(event) {
     const typeOfBuff = event.target.id
     if (typeOfBuff === "potion") {
-        MsgElement.innerText = "Heals 30HP, Number of usage left : " + potion.numberOfUsage
+        MsgElement.innerText = "Heals 30HP, Number of usage left : " + potion.numberOfUsage;
     } else if (typeOfBuff === "buff-attack") {
-        MsgElement.innerText = "Gives 25% attack buff for 1 turn, Number of usage left : " + attackBuff.numberOfUsage
+        MsgElement.innerText = "Gives 25 attack buff for 1 turn, Number of usage left : " + attackBuff.numberOfUsage;
     } else if (typeOfBuff === "accuracy") {
-        MsgElement.innerText = "Gives 25% accuracy buff for 1 turn, Number of usage left : " + accuracyBuff.numberOfUsage
+        MsgElement.innerText = "Gives 25% accuracy buff for 1 turn, Number of usage left : " + accuracyBuff.numberOfUsage;
     }
 };
 
@@ -98,7 +98,7 @@ let oppoTurn = false;
 attackButton.forEach(button => {
     button.addEventListener('click', event => {
     const typeOfAttack = event.target.id
-    if (playerTurn === true && typeOfAttack === "spit") {
+    if (playerTurn === true && typeOfAttack === "spit" && spitAttack.numberOfUsage > 0) {
         generateNumber();
         if (spitAttack.accuracy >= randomAccuracy) {
             MsgElement.innerText = "DIGGLETT ATTACKED WITH SPIT!";
@@ -108,7 +108,12 @@ attackButton.forEach(button => {
             MsgElement.innerText = "DIGGLETT HAS MISSED!";
             spitAttack.numberOfUsage -= 1;
         }
-    } else if (playerTurn === true && typeOfAttack === "sand") {
+    } else if (spitAttack.numberOfUsage <= 0 && typeOfAttack === "spit") {
+        MsgElement.innerText = "THIS ATTACK IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;
+    };
+
+    if (playerTurn === true && typeOfAttack === "sand" && sandAttack.numberOfUsage > 0) {
         generateNumber();
         if (sandAttack.accuracy >= randomAccuracy) {
             MsgElement.innerText = "DIGGLETT ATTACKED WITH SAND!";
@@ -118,7 +123,12 @@ attackButton.forEach(button => {
             MsgElement.innerText = "DIGGLETT HAS MISSED!"
             sandAttack.numberOfUsage -= 1;
         }        
-    } else if (playerTurn === true && typeOfAttack === "earthquake") {
+    } else if (sandAttack.numberOfUsage <= 0 && typeOfAttack === "sand") {
+        MsgElement.innerText = "THIS ATTACK IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;
+    };
+
+    if (playerTurn === true && typeOfAttack === "earthquake" && earthquakeAttack.numberOfUsage > 0) {
         generateNumber();
         if (earthquakeAttack.accuracy >= randomAccuracy) {
             MsgElement.innerText = "DIGGLETT ATTACKED WITH EARTHQUAKE!";
@@ -128,7 +138,12 @@ attackButton.forEach(button => {
             MsgElement.innerText = "DIGGLETT HAS MISSED!";
             earthquakeAttack.numberOfUsage -= 1;
         }        
-    } else if (playerTurn === true && typeOfAttack === "sandstorm") {
+    } else if (earthquakeAttack.numberOfUsage <= 0 && typeOfAttack === "earthquake") {
+        MsgElement.innerText = "THIS ATTACK IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;
+    };
+
+    if (playerTurn === true && typeOfAttack === "sandstorm" && sandstormAttack.numberOfUsage > 0) {
         generateNumber();
         if (sandstormAttack.accuracy >= randomAccuracy) {
             MsgElement.innerText = "DIGGLETT ATTACKED WITH SANDSTORM!";
@@ -138,12 +153,18 @@ attackButton.forEach(button => {
             MsgElement.innerText = "DIGGLETT HAS MISSED!";
             sandstormAttack.numberOfUsage -= 1;
         }          
+    } else if (sandstormAttack.numberOfUsage <= 0 && typeOfAttack === "sandstorm") {
+        MsgElement.innerText = "THIS ATTACK IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;
     }
     playerTurn = false;
     oppoTurn = true;
 
     //Removes mouseover eventListener
     removeListener();
+
+    //Remove buff if active
+    removeBuffAfterTurn();
 
     //Annoucing player's turn after 4 seconds
     setTimeout(announceTurn,4000);
@@ -260,4 +281,85 @@ function announceTurn() {
  } else if (oppoTurn === false) {
     MsgElement.innerText = "DIGGLETT'S TURN.";
  }
+};
+
+//Declaring buff's status
+let checkBuffStatus = false;
+
+//Adding of 'click' eventListeners to buff buttons
+buffButton.forEach(button => {
+    button.addEventListener('click', event => {
+    const typeOfBuff = event.target.id;
+    if (playerTurn === true && typeOfBuff === "potion" && potion.numberOfUsage > 0 && userHealth.value < 100) {
+        userHealth.value += potion.heal;
+        potion.numberOfUsage -= 1;
+        MsgElement.innerText = "YOU HAVE USED POTION ON DIGGLETT!";
+    } else if (potion.numberOfUsage <= 0 && typeOfBuff === "potion") {
+        MsgElement.innerText = "THIS BUFF IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;        
+    } else if (typeOfBuff === "potion" && userHealth.value === 100) {
+        MsgElement.innerText = "DIGGLETT'S HEALTH IS ALREADY AT 100! PLEASE CHOOSE ANOTHER OPTION!";
+        return;
+    };
+
+    if (playerTurn === true && checkBuffStatus === true && (typeOfBuff === "buff-attack" || typeOfBuff === "accuracy")) {
+        MsgElement.innerText = "BUFF IS ALREADY AND ONLY ACTIVE FOR THIS ROUND! PLEASE CHOOSE ANOTHER OPTION!"
+        return;
+    };    
+
+    if (playerTurn === true && typeOfBuff === "buff-attack" && attackBuff.numberOfUsage > 0 && checkBuffStatus === false) {
+        spitAttack.damage += attackBuff.buffAttack;
+        sandAttack.damage += attackBuff.buffAttack;
+        earthquakeAttack.damage += attackBuff.buffAttack;
+        sandstormAttack.damage += attackBuff.buffAttack;
+        attackBuff.numberOfUsage -= 1;
+        MsgElement.innerText = "YOU HAVE USED ATTACK BUFF ON DIGGLETT!";
+        checkBuffStatus = true;
+    } else if (attackBuff.numberOfUsage <= 0 && typeOfBuff === "buff-attack" && checkBuffStatus === false) {
+        MsgElement.innerText = "THIS BUFF IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;        
+    };
+
+    if (playerTurn === true && typeOfBuff === "accuracy" && accuracyBuff.numberOfUsage > 0 && checkBuffStatus === false) {
+        sandAttack.accuracy += accuracyBuff.buffAccuracy;
+        earthquakeAttack.accuracy += accuracyBuff.buffAccuracy;
+        sandstormAttack.accuracy += accuracyBuff.buffAccuracy;
+        accuracyBuff.numberOfUsage -= 1;
+        MsgElement.innerText = "YOU HAVE USED ACCURACY BUFF ON DIGGLETT!";
+        checkBuffStatus = true;
+    } else if (accuracyBuff.numberOfUsage <= 0 && typeOfBuff === "accuracy" && checkBuffStatus === false) {
+        MsgElement.innerText = "THIS BUFF IS NO LONGER USABLE! PLEASE CHOOSE ANOTHER OPTION!";
+        return;        
+    };
+    
+    playerTurn = false;
+    oppoTurn = true;
+
+    //Removes mouseover eventListener
+    removeListener();
+
+    //Annoucing player's turn after 4 seconds
+    setTimeout(announceTurn,4000);
+
+    //Opponents randomly selects an attack of its own and apply it onto userhealth.value
+    setTimeout(oppoAttack,8000);
+    });
+});
+
+//Function to check if buff is active on the next turn, and remove it after that turn
+function removeBuffAfterTurn() {
+    if (checkBuffStatus === true) {
+        if (sandstormAttack.accuracy !== 25) {
+            sandAttack.accuracy -= accuracyBuff.buffAccuracy;
+            earthquakeAttack.accuracy -= accuracyBuff.buffAccuracy;
+            sandstormAttack.accuracy -= accuracyBuff.buffAccuracy;
+            checkBuffStatus = false;    
+        } else if (sandstormAttack.damage !== 50) {
+            spitAttack.damage = 10;
+            sandAttack.damage = 20;
+            earthquakeAttack.damage = 30;
+            sandstormAttack.damage = 50;
+            checkBuffStatus = false;           
+        };
+    }
 };
